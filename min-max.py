483abdf5eefa7f26d48a2e7d4f94e1633ca8f050
Miran Tuhtan <miran.tuhtan+git@gmail.com>
2017-02-16 20:18:14 +0100

    min-max

---
+++ min-max.py
@@ -0,0 +1,18 @@
+def min(*args, **kwargs):
+    key = kwargs.get("key", None)
+    return sorted(args if len(args) > 1 else args[0], **kwargs)[0]
+
+
+def max(*args, **kwargs):
+    key = kwargs.get("key", None)
+    return min(*args, **kwargs, reverse=True)
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert max(3, 2) == 3, "Simple case max"
+    assert min(3, 2) == 2, "Simple case min"
+    assert max([1, 2, 0, 3, 4]) == 4, "From a list"
+    assert min("hello") == "e", "From string"
+    assert max(2.2, 5.6, 5.9, key=int) == 5.6, "Two maximal items"
+    assert min([[1, 2], [3, 4], [9, 0]], key=lambda x: x[1]) == [9, 0], "lambda key"
