2cf3dba4144a0efee8effb85c9fcd51f685475dc
narimiran <miran.tuhtan+git@gmail.com>
2017-04-25 08:08:55 +0200

    cookies

---
+++ cookies.py
@@ -0,0 +1,10 @@
+def get_cookie(cookie, name):
+    return next(c.split('=')[1] for c in cookie.split('; ')
+                if c.split('=')[0] == name)
+
+
+if __name__ == "__main__":
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert get_cookie('theme=light; sessionToken=abc123', 'theme') == 'light', 'theme=light'
+    assert get_cookie('_ga=GA1.2.447610749.1465220820; _gat=1; ffo=true', 'ffo') == 'true', 'ffo=true'
+    print("Looks like you know everything. It is time for 'Check'!")
