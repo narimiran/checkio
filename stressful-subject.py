0fc13d46d7c4d0369cd999591ef5edce41898878
narimiran <miran.tuhtan+git@gmail.com>
2017-05-07 17:52:57 +0200

    word is iterable

--- stressful-subject.py
+++ stressful-subject.py
@@ -3,7 +3,7 @@ import re
 def is_stressful(subj):
     return (subj.isupper() or
             subj.endswith('!!!') or
-            any(re.search('+[.!-]*'.join(c for c in word), subj.lower())
+            any(re.search('+[.!-]*'.join(word), subj.lower())
                 for word in ['help', 'asap', 'urgent']))
 
 
